7037effc1f481a1de5ca168755912ec258399fa4
Mythamor <bethmithamo.bm@gmail.com>
2023-09-07 22:24:32 +0300

    Creating an archive with fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script that generates .tgz archive from contents of the web_static 
+Fabric script that generates .tgz archive from contents of the web_static
 folder of your AirBnB Clone repo, using the function do_pack
 """
 from fabric.api import local
