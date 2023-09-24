d68b91a5d84dc2f22d79352c6526b6eb8ffbdf95
Mythamor <bethmithamo.bm@gmail.com>
2023-09-24 13:45:23 +0300

    Setting up flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,6 +9,7 @@ You must use the option strict_slashes=False in your route definition
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ display message"""
